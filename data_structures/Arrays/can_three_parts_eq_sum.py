66b6fbc3186dcb90b4e0a4f75d784fdfe9d4c117
Jonathan Huth <jonhuth95@gmail.com>
2021-02-22 17:18:21 -0800

    added soln. for partition array into 3 parts w/ equal sums

---
+++ data_structures/Arrays/can_three_parts_eq_sum.py
@@ -0,0 +1,17 @@
+def canThreePartsEqualSum(self, arr):
+    # time: O(n) | space: O(1)
+    totalSum = sum(arr)
+    if totalSum % 3 != 0:
+        return False
+    targetSum = totalSum // 3
+
+    cumulativeSum = counter = 0
+    for num in arr:
+        if counter == 2:
+            return True
+
+        cumulativeSum += num
+        if cumulativeSum == targetSum:
+            cumulativeSum = 0
+            counter += 1
+    return False
