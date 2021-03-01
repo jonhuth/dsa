eefe4d2e23b6d5aea727036ea57769479784e320
Jonathan Huth <jonhuth95@gmail.com>
2021-03-01 14:50:18 -0800

    updated getBit fn

--- Bit Manipulation/get_bit.py
+++ Bit Manipulation/get_bit.py
@@ -1,4 +1,5 @@
 def getBit(num, idx):
     # get the bit at the i-th idx
+    # num (shift_left, shift_right) how many idxs over
     bitMask = 1 << idx
-    return num & bitMask
+    return (num & bitMask) != 0
