4b91d6fdd105cae725661ef7ee9b29e37cb607c9
Jonathan Huth <jonhuth95@gmail.com>
2021-03-02 21:16:57 -0800

    fixed issue with fn. returning (n-1)th fib instead of nth

--- Math/get_nth_fib.py
+++ Math/get_nth_fib.py
@@ -3,7 +3,7 @@ def getNthFib(n):
     # argument: 1,2...n
     # worst case: O(n) time | O(1) space
     # last two fib nums stored here
-    fibs = [0, 1]
+    fibs = [0, 1]  # last two fibs (i-2, i-1)
     for i in range(2, n):
         fibs[0], fibs[1] = fibs[1], fibs[0] + fibs[1]
-    return fibs[1] if n > 1 else fibs[0]
+    return fibs[n] if n in [0, 1] else fibs[0] + fibs[1]
