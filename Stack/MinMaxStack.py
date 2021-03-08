9cdf08a5912997c475b713bdaa927bb18a2cbf91
jonhuth <jonhuth95@gmail.com>
2021-03-07 23:49:42 -0800

    added min max stack class w/ methods

---
+++ Stack/MinMaxStack.py
@@ -0,0 +1,28 @@
+# Feel free to add new properties and methods to the class.
+class MinMaxStack:
+    def __init__(self):
+        self.stack = []  # [a, b, c, ...]
+        self.minMaxStack = []  # [{'min': x, 'max': y}, ...]
+        # [5]
+        # [{'min': 5, 'max': 5}]
+    def peek(self):
+        return self.stack[-1]
+
+    def pop(self):
+        self.minMaxStack.pop()
+        return self.stack.pop()
+
+    def push(self, number):
+        newMinMax = {'min': number, 'max': number}
+        if self.minMaxStack:
+            lastMinMax = self.minMaxStack[-1]
+            newMinMax['min'] = min(lastMinMax['min'], number)
+            newMinMax['max'] = max(lastMinMax['max'], number)
+        self.stack.append(number)
+        self.minMaxStack.append(newMinMax)
+        
+    def getMin(self):
+        return self.minMaxStack[-1]['min']
+
+    def getMax(self):
+        return self.minMaxStack[-1]['max']
