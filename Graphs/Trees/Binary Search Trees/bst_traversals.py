046995a2f1faf7826eed043bca0bbf3886cb859d
jonhuth <jonhuth95@gmail.com>
2021-03-07 23:04:50 -0800

    added bst traversal functions

---
+++ Graphs/Trees/Binary Search Trees/bst_traversals.py
@@ -0,0 +1,26 @@
+# time: O(n) | space: O(n)
+def inOrderTraverse(tree, array):
+    if tree is None:
+        return
+    inOrderTraverse(tree.left, array)
+    array.append(tree.value)
+    inOrderTraverse(tree.right, array)
+    return array
+
+
+def preOrderTraverse(tree, array):
+    if tree is None:
+        return
+    array.append(tree.value)
+    preOrderTraverse(tree.left, array)
+    preOrderTraverse(tree.right, array)
+    return array
+
+
+def postOrderTraverse(tree, array):
+    if tree is None:
+        return
+    postOrderTraverse(tree.left, array)
+    postOrderTraverse(tree.right, array)
+    array.append(tree.value)
+    return array
