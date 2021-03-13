803bf1eb89d273f9cc56418b52065e77fa5912da
jonhuth <jonhuth95@gmail.com>
2021-03-12 23:40:57 -0800

    added soln. to find avg of values in each level of binary tree

---
+++ Graphs/Trees/avg_of_levels.py
@@ -0,0 +1,43 @@
+# Definition for a binary tree node.
+from collections import deque
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+def averageOfLevels(root):
+    '''
+    bfs style soln.
+    1. init avgs arr & queue (starts with root at front)
+    2. while queue not empty, init lvlSum and lvlCount both to 0
+    3. make new empty temporary queue
+    4. while queue not empty, popleft curr node
+    5. lvlSum += curr.val and lvlCount += 1
+    6. if curr node as left child append it to temporary q
+    7. if curr nod has right child append to temporary q
+    8. once original q empty, reassign q to temp q
+    9. append the lvl avg to output array
+    10. repeat until original q is empty then return output arr
+
+    time: O(n) | space: O(w) where n = num nodes and m is max width of tree (bottom level)
+    '''
+    avgs = []
+    q = deque([root])
+    while q:
+        lvlSum, lvlCount = 0, 0
+        tempQ = deque([])
+        while q:
+            curr = q.popleft()
+            lvlSum += curr.val
+            lvlCount += 1
+            if curr.left:
+                tempQ.append(curr.left)
+            if curr.right:
+                tempQ.append(curr.right)
+        q = tempQ
+        avgs.append(lvlSum / lvlCount)
+    return avgs
