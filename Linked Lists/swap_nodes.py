9a8999c30ebeed0726fce89fc07245aba0ad39ec
jonhuth <jonhuth95@gmail.com>
2021-03-20 01:51:56 -0700

    added soln. to swap node vals in LL

---
+++ Linked Lists/swap_nodes.py
@@ -0,0 +1,24 @@
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+def swapNodes(head: ListNode, k: int) -> ListNode:
+    # time: O(n) | space: O(1)
+    n = 0
+    curr = head
+    while curr:
+        curr = curr.next
+        n += 1
+
+    left = head
+    for i in range(k - 1):
+        left = left.next
+
+    right = head
+    for i in range(n - k):
+        right = right.next
+
+    right.val, left.val = left.val, right.val
+    return head
