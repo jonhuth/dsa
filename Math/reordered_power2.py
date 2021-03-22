879f0c98442dbd6047edd83688524d5bab38f6f0
jonhuth <jonhuth95@gmail.com>
2021-03-21 23:36:26 -0700

    added soln. for reordered power of 2

---
+++ Math/reordered_power2.py
@@ -0,0 +1,24 @@
+from collections import Counter
+
+
+def reorderedPowerOf2(N: int) -> bool:
+    """
+    naive soln. - enumerate all permutations
+    time: O(n*n!) | space O(n*n!) where n is num digits in N
+
+    1. get all permutations of N
+    2. throw out ones that have leading 0
+    3. for each perm: check if power of 2 using: ((n & (n-1) == 0) and n != 0) => true
+    4. otherwise return false at end
+
+    optimal soln. - check set of digits for N against set of digits for all power of 2 nums
+    time: O(d) | space: O(d) - where d is num digits in N
+    note: if N is power of 10, cannot get power of 2, return false
+    """
+    N_counter = Counter(str(N))
+    for i in range(30):
+        power2_counter = Counter(str(1 << i))
+        if N_counter == power2_counter:
+            return True
+
+    return False
