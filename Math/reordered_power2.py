c7cd002d156b90e9bbdf40f4dad069577edbb0aa
jonhuth <jonhuth95@gmail.com>
2021-03-21 23:50:57 -0700

    updated time and space complexity for reordered power of 2

--- Math/reordered_power2.py
+++ Math/reordered_power2.py
@@ -3,6 +3,9 @@ from collections import Counter
 
 def reorderedPowerOf2(N: int) -> bool:
     """
+    prompt: given integer N, find if its possible to reorder digits in N to make a power of 2.
+    leading digit cannot be 0.
+
     naive soln. - enumerate all permutations
     time: O(n*n!) | space O(n*n!) where n is num digits in N
 
@@ -12,7 +15,7 @@ def reorderedPowerOf2(N: int) -> bool:
     4. otherwise return false at end
 
     optimal soln. - check set of digits for N against set of digits for all power of 2 nums
-    time: O(d) | space: O(d) - where d is num digits in N
+    time: O((logn)^2) | space: O(logn)
     note: if N is power of 10, cannot get power of 2, return false
     """
     N_counter = Counter(str(N))
