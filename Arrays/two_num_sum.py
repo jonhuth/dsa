7c370e2b0d0b35f68cec828fd2d506f2659bba7b
jonhuth <jonhuth95@gmail.com>
2021-03-24 16:46:09 -0700

    added two sum prob soln.

---
+++ Arrays/two_num_sum.py
@@ -0,0 +1,11 @@
+def twoNumberSum(array, targetSum):
+    # time: O(n) | space: O(n)
+    s = set()
+
+    for e in array:
+        complement = targetSum - e
+        if complement in s:
+            return [complement, e]  # found pair
+        else:
+            s.add(e)
+    return []  # didn't find pair
