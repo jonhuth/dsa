b93b547fb8ad5795f41a0a2b3b805493b562abab
jonhuth <jonhuth95@gmail.com>
2021-03-25 15:33:44 -0700

    updated sort_k_messed_array soln.

--- Sorting/sort_k_messed_array.py
+++ Sorting/sort_k_messed_array.py
@@ -4,8 +4,10 @@ def sort_k_messed_array(arr, k):
      i
     [1, 2, 3,4, 5, 6, 7, 8 ,9 ,10]
      j   
-     notes:
-     assume k small relative to n (like constant)
+    notes:
+    - assume k small relative to n (like constant)
+    - uses variant of selection sort
+
 
      soln. steps:
      1. iterate through arr
@@ -19,9 +21,16 @@ def sort_k_messed_array(arr, k):
 
     for i in range(len(arr)):
         offset = 1
+        min_idx = i
         while i + offset < len(arr) and offset <= k:
             if arr[i] > arr[i + offset]:
-                arr[i], arr[i + offset] = arr[i + offset], arr[i]
+                min_idx = i + offset
             offset += 1
+        arr[i], arr[min_idx] = arr[min_idx], arr[i]
 
     return arr
+
+
+arr = [1, 4, 5, 2, 3, 7, 8, 6, 10, 9]
+k = 2
+print(sort_k_messed_array(arr, k))
