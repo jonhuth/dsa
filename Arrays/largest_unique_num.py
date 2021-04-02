a1d340b0c674f2e3bb1ac86096701dbe049eba23
jonhuth <jonhuth95@gmail.com>
2021-04-01 21:19:41 -0700

    added soln. for largest unique num

---
+++ Arrays/largest_unique_num.py
@@ -0,0 +1,15 @@
+def largestUniqueNumber(A: list[int]) -> int:
+    # counter based soln: time: O(n) | O(n)
+    # time: O(nlogn) | space: O(1)
+    A.sort()
+    A.append('dummy')
+
+    for i in range(len(A) - 2, -1, -1):
+        if A[i-1] != A[i] and A[i+1] != A[i]:
+            return A[i]
+
+    return -1
+
+
+arr1 = [1, 2, 2, 3, 4, 4]
+print(largestUniqueNumber(arr1))
