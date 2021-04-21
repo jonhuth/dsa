b8512eb2535ccaeeb9ed95355b132804b6926ff9
jonhuth <jonhuth95@gmail.com>
2021-04-21 00:35:30 -0700

    added Priority Queue boilerplate

---
+++ Priority Queue/MaxPQ.py
@@ -0,0 +1,18 @@
+class MaxPQ:
+    def __init__(self):
+        self.heap = []
+
+    def insert(self, v):
+        pass
+
+    def del_max(self):
+        pass
+
+    def is_empty(self):
+        pass
+
+    def max(self):
+        pass
+
+    def size(self):
+        pass
