3344937a2def0d0bb9446ffd735607aa89b62845
jonhuth <jonhuth95@gmail.com>
2021-05-09 05:07:51 -0700

    refined array of products soln.

---
+++ Arrays/array_of_products.py
@@ -0,0 +1,47 @@
+def array_of_products(arr):
+    '''
+    ex 1: arr = [8, 10, 2] => [20, 16, 80]
+    left: [1, 8, 80] left to right
+    right: [20, 2, 1] right to left
+    out: [20, 16, 80]
+
+    ex 2: arr = [0, 2, 5] => [10, 0, 0]
+    left: [1, 0, 0] left to right
+    right: [10, 5, 1] right to left
+    out: [10, 0, 0]
+
+    This is the key:
+    -> left_arr[i] * right_arr[i] for all i
+    time: O(n) | space: O(n)
+    '''
+    out = [1] * len(arr)
+    left_prod = right_prod = 1
+    for i in range(1, len(arr)):
+        left_prod *= arr[i-1]
+        right_prod *= arr[-1-i+1]
+        out[i] *= left_prod
+        out[-1-i] *= right_prod
+    return out
+    # out = [1] * len(arr)
+    # left_prod = right_prod = 1
+    # for i in range(1, len(arr)):
+    #     left_prod *= arr[i-1]
+    #     out[i] *= left_prod
+    # for i in range(len(arr) - 2, -1, -1):
+    #     right_prod *= arr[i+1]
+    #     out[i] *= right_prod
+    # return out
+
+    # if len(arr) <= 1:
+    #     return []
+
+    # left_arr = [1] + [0] * (len(arr) - 1)  # [1, 0, 0, ...] len=n
+    # right_arr = [0] * (len(arr) - 1) + [1]  # [0, 0, ... , 1] len = n
+
+    # for i in range(1, len(arr)):
+    #     left_arr[i] = arr[i-1] * left_arr[i-1]
+
+    # for i in range(len(arr)-2, -1, -1):
+    #     right_arr[i] = arr[i+1] * right_arr[i+1]
+
+    # return [left_arr[i] * right_arr[i] for i in range(len(arr))]
