25998df96d9780b1472423d443142603de16a151
jonhuth <jonhuth95@gmail.com>
2021-05-12 21:11:42 -0700

    added median of medians soln.

---
+++ Sorting/median_of_medians.py
@@ -0,0 +1,23 @@
+def median_of_medians(A, i):
+    '''
+    find ith smallest number in A.
+
+    time: O(n) | space: O(n)
+    '''
+    sublists = [A[j:j+5] for j in range(0, len(A), 5)]
+    medians = [sorted(sublist)[len(sublist)//2] for sublist in sublists]
+    if len(medians) <= 5:
+        pivot = sorted(medians)[len(medians)//2]
+    else:
+        pivot = median_of_medians(medians, len(medians)//2)
+
+    low = [j for j in A if j < pivot]
+    high = [j for j in A if j > pivot]
+
+    k = len(low)
+    if i < k:
+        return median_of_medians(low, i)
+    elif i > k:
+        return median_of_medians(high, i - k - 1)
+    else:
+        return pivot
