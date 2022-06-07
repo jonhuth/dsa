cda1c22ecbc85fb55016da78388c4e955320baf6
jonhuth <jonhuth95@gmail.com>
2022-06-07 15:16:35 -0700

    updated group anagrams soln.

--- HashTables/group_anagrams.py
+++ HashTables/group_anagrams.py
@@ -1,14 +1,15 @@
+from collections import defaultdict
 def groupAnagrams(words):
     '''
-    time: O(w*n*log(n)) | space: O(w*n) w=num of words and n= longest word len
+    time: O(w*n*log(n)) | space: O(w) w=num of words and n= longest word len
     ex: [yo, act, flop, tac, foo, cat, oy, olfp] =>
     [oy, act, flop, act, foo, act, oy, flop]
     {'oy': ['yo', 'oy'], 'act': ['act', 'cat'], 'flop': ['flop', 'olfp'],
      'foo': ['foo'] }
     => [['yo', 'oy'], ['act', 'cat'], ['flop', 'olfp'], ['foo']]
     '''
-    wordsSorted = [''.join(sorted(word)) for word in words]
-    wordsDict = {}  # k: v => sortedWord: [assocWord0, assocWord1, ...]
-    for i, word in enumerate(wordsSorted):
-        wordsDict[word] = wordsDict.get(word, []) + [words[i]]
-    return list(wordsDict.values())
+    wordsDict = defaultdict(list)  # k: v => sortedWord: [assocWord0, assocWord1, ...]
+    for word in words:
+        sortedWord = str(sorted(word))
+        wordsDict[sortedWord].append(word)
+    return wordsDict.values()
