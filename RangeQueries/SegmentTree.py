485089f58adc2fe6e4e8b0d708ffde765e1e0fb7
Jonathan Huth <jonhuth95@gmail.com>
2022-07-25 21:59:07 -0700

    SegmentTree skeleton

---
+++ RangeQueries/SegmentTree.py
@@ -0,0 +1,10 @@
+class SegmentTree:
+    
+    def __init__(self, nums):
+        self.segmentTree = [0] * 4 * len(nums)
+
+    def update(self, ):
+        pass
+
+    def query(self, i, j):
+        pass
